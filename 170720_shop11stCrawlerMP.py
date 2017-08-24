6ca2baf2c938fff61211ecfbe3395e7384b70e9a
Soo <soominjeong93@gmail.com>
2017-08-24 17:35:21 +0900

    Create 170720_shop11stCrawlerMP.py

---
+++ 170720_shop11stCrawlerMP.py
@@ -0,0 +1,45 @@
+#The multiprocessing version
+#designed by Soo Min, Jeong
+
+from shop11st_Crawler_v6 import access
+from selenium import webdriver
+
+from time import time, strftime
+from multiprocessing import Pool
+
+def main(args):
+    #URL crawling
+    start =0
+    driver = webdriver.Chrome()
+    driver.get(args[1])
+    box_pd = driver.find_element_by_id('layBodyWrap').find_elements_by_class_name('box_pd')
+
+    totalReviews = 0
+
+    for eachDeal in box_pd[50:]:
+        url = eachDeal.find_element_by_tag_name('a').get_attribute('href')
+        args[1] = url
+        totalReviews += access(args)
+        print ("accumulative:", totalReviews)
+
+
+def fileOpening(num):
+    start_time = time()
+    args = []
+    siteInput = open(str(num)+"_11st_Soo_v1.txt", "r")
+
+    for line in siteInput:
+        lineArray = line.split()
+        args.append(lineArray)
+    siteInput.close()
+
+    pool = Pool(processes=1)
+    pool.map(main, args)
+
+    #for a single process
+    #main(args)
+    print("--- %s seconds ---", (time.time()- start_time))
+
+for num in range(1,13):
+    fileOpening(num)
+
